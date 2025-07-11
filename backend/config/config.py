e6af26435091688d61567c6b43ec894bdb54385e
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2025-07-11 09:39:27 +0545

    bump token expire time
    Signed-off-by: Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>

--- backend/config/config.py
+++ backend/config/config.py
@@ -17,7 +17,7 @@ class Settings:
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 30  # mins
+    ACCESS_TOKEN_EXPIRE_MINUTES = 1440  # mins
 
 
 settings = Settings()
